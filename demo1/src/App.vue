95c3a70242e16e08bc3454aff8f2609d69019eed
sunmeng2019 <805573059@qq.com>
2019-09-02 11:21:15 +0800

    提交代码

---
+++ demo1/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <button type="submit" @click="add">+</button>
+    <button type="submit" @click="minus">-</button>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data: function() {
+    return {
+      message: 0
+    };
+  },
+  methods: {
+    add: function() {
+      this.message++;
+      // document.getElementById("message").value=this.message
+      console.log(this.message);
+    },
+    minus: function() {
+      this.message--;
+      // document.getElementById("message").value=this.message
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ demo1/src/components/counter.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+<button type="submit" @click="add">+</button>
+<p> {{message}}</p > 
+<button type="submit" @click="minus">-</button>
+</div>
+<script>
+
+
+export default {
+created:function(){
+this.message = 0;
+},
+
+methods:{
+add:function () {
+this.message++;
+<!-- document.getElementById("message").value=this.message -->
+},
+minus:function () {
+this.message--;
+<!-- document.getElementById("message").value=this.message -->
+}
+}
+}
+</script>
+<style>
+
+</style>
